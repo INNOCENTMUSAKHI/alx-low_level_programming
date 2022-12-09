82587bf1f55e08bb41b0ed2b166ef36ab3ed35f7
INNOCENTMUSAKHI <innocentmusakhi@gmail.com>
2022-12-08 20:59:01 -0800

    space

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,11 @@
  * main - Entry point
  * Return: Always o (Success)
  */
+
 int main (void)
 {
+
 	puts("\" Programming is like building a multilingual puzzle");
-		return (0);
+				return (0);
+
 }
