8a80d4e223f6a2c4900fbcd55d724f67a4c02b56
INNOCENTMUSAKHI <innocentmusakhi@gmail.com>
2022-12-16 05:12:53 -0800

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagonal - check for a digit
+ * @n: number of \\ to be printed
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i = 0, ii;
+
+	while (i < n && n > 0)
+	{
+		ii = 0;
+		while (ii < i)
+		{
+			putchar(',');
+			ii++;
+		}
+		putchar('\\');
+		putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		putchar('\n');
+}
