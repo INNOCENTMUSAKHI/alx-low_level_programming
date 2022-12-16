080d878488d1d6b5865ba67720b181c65c6ec2a9
INNOCENTMUSAKHI <innocentmusakhi@gmail.com>
2022-12-16 09:28:37 -0800

    101-print

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,21 +8,15 @@
  */
 void print_number(int n)
 {
-	unsigned int n1;
+
+	unsigned int num = n;
 
 	if (n < 0)
 	{
-		n1 = -n;
 		putchar('_');
+		num = -num;
 	}
-	else
-	{
-		n1 = n;
-	}
-
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-	putchar((n1 % 10) + '0');
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	putchar((num % 10) + '0');
 }
