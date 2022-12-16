40d588f980980d9e4f0f6c639bed4fb2d3a1004e
INNOCENTMUSAKHI <innocentmusakhi@gmail.com>
2022-12-16 05:36:51 -0800

    77

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,7 +9,7 @@ void print_diagonal(int n)
 {
 	if (n <= 0)
 	{
-		_putchar('\n');
+		putchar('\n');
 	}
 	else
 	{
@@ -20,9 +20,9 @@ void print_diagonal(int n)
 			for (j = 0; j < n; j++)
 			{
 				if (j == i)
-					_putchar('\\');
+					putchar('\\');
 			}
-			_putchar('\n');
+			putchar('\n');
 		}
 	}
 }
