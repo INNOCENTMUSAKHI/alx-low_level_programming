1a9dfd667db0e1d37f3b29046e468d9c32aa06e3
INNOCENTMUSAKHI <innocentmusakhi@gmail.com>
2022-12-19 12:50:34 -0800

    777

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,31 +7,16 @@
  */
 void puts_half(char *str)
 {
-	int len, n, i;
+	int i;
 
-	len = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		;
 
-	while
-		(str[len] != '\0');
-
-{
-		 len++;
-}
-
-	if (len % 2 == 0)
+	i++;
+	for (i /= 2; str[i]; != '\0'; i++)
 	{
-		for (i = len / 2; str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
-	}
-	else if (len % 2)
-	{
-		for (n = (len - 1) / 2; n < len - 1; i++)
-		{
-			_putchar(str[n + 1]);
-		}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
-}
 
+}
